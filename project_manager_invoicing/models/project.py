ef50e7793a7781c384f61ab35abe5197a356e7ac
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2015-09-10 11:24:35 +0200

    Replace api 8.0 field by old api due to bug #odoo/8461

--- project_manager_invoicing/models/project.py
+++ project_manager_invoicing/models/project.py
@@ -16,26 +16,46 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from openerp import models, fields, api
+from openerp import models, fields, api, osv
 
 
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    invoiced_hours = fields.Float(compute='_get_invoiced_hours', store=True)
+    # Due to https://github.com/odoo/odoo/issues/8461
+    # we cannot use api 8.0 field
+    @api.multi
+    def _get_invoiced_hours(self, names, args):
+        """ Sum timesheet line invoiced hours """
+        res = {}
+        for task in self:
+            res[task.id] = sum(l.invoiced_hours for l in self.work_ids)
+        return res
+
+    @api.multi
+    def _get_analytic_line(self):
+        result = []
+        for aal in self:
+            if aal.task_id:
+                result.append(aal.task_id.id)
+        return result
+
+    _columns = {
+        'invoiced_hours': osv.fields.function(
+            _get_invoiced_hours,
+            type='float',
+            store={'project.task': (lambda self, cr, uid, ids, c=None: ids,
+                                    ['work_ids'], 20),
+                   'account.analytic.line': (_get_analytic_line,
+                                             ['task_id', 'invoiced_hours'], 20)
+                   }
+        ),
+    }
+
     remaining_hours = fields.Float(
         compute='_get_remaining_hours', store=True,
         help="Total remaining time based on invoiced hours"
     )
-
-    @api.one
-    # This would pervent creation of timesheet lines
-    # as task_id (counter part of work_ids) is defined on the analytic line
-    # where as the work_ids is defined on hr.analytic.timesheet
-    @api.depends('work_ids')
-    def _get_invoiced_hours(self):
-        """ Sum timesheet line invoiced hours """
-        self.invoiced_hours = sum(l.invoiced_hours for l in self.work_ids)
 
     @api.one
     @api.depends('planned_hours', 'invoiced_hours')
